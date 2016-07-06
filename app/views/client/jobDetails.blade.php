cf3c8d5f8c1bfc86cb2d3fc365e418546151bad2
jjsarmiento <sarmiento11102@gmail.com>
2016-07-06 15:40:15 +0800

    git status

--- app/views/client/jobDetails.blade.php
+++ app/views/client/jobDetails.blade.php
@@ -20,6 +20,17 @@
             min-width: 100%;
             height:1px;
         }
+
+        .applicant-container {
+            min-height: 1em;
+            border-bottom:
+            #ECF0F1 1px solid;
+            /*transition: 0.3s;*/
+        }
+
+        .applicant-container:hover {
+            background-color: #F0FFFF;
+        }
     </style>
 @stop
 
@@ -80,8 +91,35 @@
             </div>
         </div>
         <div class="col-md-4">
-            <div class="widget-container padded" style="display: flex; min-height:125px; display:block !important;">
+            <div class="widget-container stats-container" style="display:block !important;">
+                <div class="col-lg-6 lato-text">
+                    <a id="APPLICANTSLINK" href="#" style="text-decoration:none;">
+                        <div class="number" style="color:#2980b9;">
+                            <i class="fa fa-users"></i>
+                            12
+                        </div>
+                        <div class="text" style="color:#2980b9;">
+                            Applicants
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-6 lato-text">
+                    <a id="INVITEDSLINK" href="#" style="text-decoration:none;">
+                        <div class="number" style="color:#2980b9;">
+                            <i class="fa fa-envelope-square"></i>
+                            3
+                        </div>
+                        <div class="text" style="color:#2980b9;">
+                            Invited
+                        </div>
+                    </a>
+                </div>
+            </div><br/>
 
+            <div id="APPLICANTS">
+                {{--<div class="widget-container padded applicant-container" style="">--}}
+                    {{--<h4 style="margin: 0;">J** S********</h4>--}}
+                {{--</div>--}}
             </div>
         </div>
     </div>
