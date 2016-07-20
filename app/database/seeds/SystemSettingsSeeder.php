59bbcdbae242120f6d643ff7ef64fa6a82cf24d9
jjsarmiento <sarmiento11102@gmail.com>
2016-07-20 14:37:31 +0800

    vendor files and seeder fix

--- app/database/seeds/SystemSettingsSeeder.php
+++ app/database/seeds/SystemSettingsSeeder.php
@@ -8,24 +8,20 @@
 class SystemSettingsSeeder extends Seeder {
 
     public function run(){
-        date_default_timezone_set("Asia/Manila");
 
         SystemSetting::create(array(
             'type'          =>  "SYSSETTINGS_POINTSPERAD",
-            'value'         =>  "5",
-            'created_at'    =>  date("Y:m:d H:i:s")
+            'value'         =>  "5"
         ));
 
         SystemSetting::create(array(
             'type'          =>  "SYSSETTINGS_JOBADDURATION",
-            'value'         =>  "168",
-            'created_at'    =>  date("Y:m:d H:i:s")
+            'value'         =>  "168"
         ));
 
         SystemSetting::create(array(
             'type'          =>  "SYSSETTINGS_CHECKOUTPRICE",
-            'value'         =>  "10",
-            'created_at'    =>  date("Y:m:d H:i:s")
+            'value'         =>  "10"
         ));
     }
 }
