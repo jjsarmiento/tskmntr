669f846014d84434020ec77a6c43d534f72d46b8
jjsarmiento <sarmiento11102@gmail.com>
2016-07-28 11:13:51 +0800

    modal for privacy policy

--- app/views/modals.blade.php
+++ app/views/modals.blade.php
@@ -45,11 +45,32 @@
         <div class="modal-content">
             <div class="modal-header">
                 <a href="#" data-dismiss="modal" class="close"><i class="fa fa-close"></i></a>
-                <h3>Terms of Service - Proveek BETA</h3>
-                <a href="#" data-dismiss="modal" data-target="#TERMSMODAL_TAGALOG" id="#TOS_TO_TAGALOG" data-toggle="modal">Click here for TOS Tagalog Version</a>
+                <h3>Privacy Policy - Proveek BETA</h3>
             </div>
-            <div class="modal-body" style="padding: 4em">
+            <div class="modal-body">
+                <div class="row">
+                <div class="col-md-12">
+                    <ul class="nav nav-tabs" role="tablist">
+                        <li role="presentation" class="active"><a href="#POL_ES_VERSION" aria-controls="home" role="tab" data-toggle="tab">English Version</a></li>
+                        <li role="presentation"><a href="#POL_TG_VERSION" aria-controls="profile" role="tab" data-toggle="tab">Tagalog Version</a></li>
+                    </ul>
+                </div>
 
+                <div class="tab-content">
+                    <div role="tabpanel" class="tab-pane active fade in" id="POL_ES_VERSION">
+                        <div class="col-md-12">
+                            <br/>
+                            {{SystemSetting::where('type', 'SYSSETTINGS_POLICY_ES')->pluck('value')}}
+                        </div>
+                    </div>
+                    <div role="tabpanel" class="tab-pane active fade" id="POL_TG_VERSION">
+                        <div class="col-md-12">
+                            <br/>
+                            {{SystemSetting::where('type', 'SYSSETTINGS_POLICY_TG')->pluck('value')}}
+                        </div>
+                    </div>
+                </div>
+                </div>
             </div>
         </div>
     </div>
