eefddd3bb0f6508c614070a3900eca632496aa2e
ariel.dtorres <ariel.dtorres@gmail.com>
2016-08-04 20:24:01 +0800

    admin ui

--- app/views/admin/AT_userList.blade.php
+++ app/views/admin/AT_userList.blade.php
@@ -12,6 +12,33 @@
  @section('head-content')
     <style type="text/css">
         body{background-color:#E9EAED;}
+        /* Added by Jups */
+        section{
+            background: url("../frontend/img/slideshow/10admin.jpg");
+            background-size: cover;
+            background-repeat: no-repeat;
+        }
+        h1.lato-text{
+            color: white;
+        }
+        .widget-container{
+            background-color: rgba(245,245,245,0.3);
+        }
+        .breadcrumb, .panel-heading{
+            background-color: rgba(245,245,245,0.7);
+        }
+        .breadcrumb>li{
+            color: white !important;
+        }
+        a.sidemenu {
+            color: white;
+        }
+        a.sidemenu:hover {
+            transition: 0.3s;
+            color: #d9d9d9;
+            text-decoration: none;
+        }
+        /*-----------------*/
         .accordion-toggle
         {
             text-decoration: none !important; 
