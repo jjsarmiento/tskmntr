22ebbd6e4d716cdb8661a163a3af0c3f2c46d6a4
jjsarmiento <sarmiento11102@gmail.com>
2016-08-09 18:23:35 +0800

    removed nav bar from admin job details

--- app/views/admin/ADMIN_jobDetails.blade.php
+++ app/views/admin/ADMIN_jobDetails.blade.php
@@ -51,67 +51,6 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-md-3">
-                <div class="widget-container">
-                    <div class="widget-content">
-                        <div class="panel-heading">
-                            <div class="panel-title">
-                                <a class="accordion-toggle">
-                                User Account List</a>
-                            </div>
-                        </div>
-                        <div class="panel-body">
-                            <i class="glyphicon glyphicon-chevron-right"></i> &nbsp; <a href="/admin" class="sidemenu">Pending Users</a><br>
-                            <i class="glyphicon glyphicon-chevron-right"></i> &nbsp; <a href="/userListTaskminators" class="sidemenu">Worker</a><br>
-                            <i class="glyphicon glyphicon-chevron-right"></i> &nbsp; <a href="/UsrAccntLstCMPNY" class="sidemenu">Company</a><br>
-                            <!--
-                            <i class="glyphicon glyphicon-chevron-right"></i> &nbsp; <a href="/userListClientIndi" class="sidemenu">Employer - Individuals</a><br>
-                            <i class="glyphicon glyphicon-chevron-right"></i> &nbsp; <a href="/userListClientComp" class="sidemenu">Employer - Companies</a><br>
-                            -->
-                        </div>
-                        <div class="panel-heading">
-                            <div class="panel-title">
-                                <a class="accordion-toggle">
-                                Job Ads&nbsp;&nbsp;
-                                <span id="searchAdBtn" data-target="#adSearchModal" data-toggle="modal" style="font-size:0.8em; background-color: #2980b9; border-radius: 0.8em; padding: 0.2em; padding-left: 0.5em; padding-right: 0.5em; color: #ffffff; cursor: pointer">
-                                    <i class="fa fa-search"></i> Search
-                                </span>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="panel-body">
-                            <i class="glyphicon glyphicon-chevron-right"></i> &nbsp; <a href="/showJobAds" class="sidemenu">All Job Ads</a><br>
-                            {{--<i class="glyphicon glyphicon-chevron-right"></i> &nbsp; <a href="/jobAds=INDIVIDUAL" class="sidemenu">Individual</a><br>--}}
-                            {{--<i class="glyphicon glyphicon-chevron-right"></i> &nbsp; <a href="/jobAds=FEATURED" class="sidemenu">Featured</a><br>--}}
-                            {{--<i class="glyphicon glyphicon-chevron-right"></i> &nbsp; <a href="/jobAds=HIRING" class="sidemenu">Mass Hiring</a><br>--}}
-                            {{--<i class="glyphicon glyphicon-chevron-right"></i> &nbsp; <a href="/jobAds=REFERRAL" class="sidemenu">Referral</a><br>--}}
-                        </div>
-                        <div class="panel-heading">
-                            <div class="panel-title">
-                                <a class="accordion-toggle">
-                                Audit Trail</a>
-                            </div>
-                        </div>
-                        <div class="panel-body">
-                            <i class="glyphicon glyphicon-chevron-right"></i> &nbsp; <a href="/AT_taskminator" class="sidemenu">Workers</a><br>
-                            <i class="glyphicon glyphicon-chevron-right"></i> &nbsp; <a href="/AT_companies" class="sidemenu">Company</a><br>
-                            <!--
-                            <i class="glyphicon glyphicon-chevron-right"></i> &nbsp; <a href="/AT_clientindi" class="sidemenu">Client (Individual)</a><br>
-                            <i class="glyphicon glyphicon-chevron-right"></i> &nbsp; <a href="/AT_clientcomp" class="sidemenu">Client (Company)</a>
-                            -->
-                        </div>
-                        <div class="panel-heading">
-                            <div class="panel-title">
-                                <a class="accordion-toggle">
-                                Category & Skills</a>
-                            </div>
-                        </div>
-                        <div class="panel-body">
-                            <i class="glyphicon glyphicon-chevron-right"></i> &nbsp; <a href="/skills" class="sidemenu">Manage</a><br>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <div class="col-md-9">
                 <div class="widget-container">
                     <div class="widget-content padded">
