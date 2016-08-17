97d2c4faa1300af5207368f3e7c920c4c0893031
jjsarmiento <sarmiento11102@gmail.com>
2016-08-17 15:48:03 +0800

    added container [checkouts page]

--- app/views/client/checkouts.blade.php
+++ app/views/client/checkouts.blade.php
@@ -17,26 +17,28 @@
 <section>
     <div class="container lato-text">
         <div class="col-md-8">
-            @if($workers->count() != 0)
-                <table class="table table-striped table-hover table-condensed">
-                    <thead>
-                        <th>Checkout Date</th>
-                        <th>Name</th>
-                    </thead>
-                    <tbody>
-                        @foreach($workers as $w)
-                            <tr>
-                                <td>{{$w->purchased_at}}</td>
-                                <td><a href="/{{$w->username}}">{{$w->fullName}}</a></td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            @else
-                <center><i>
-                    No workers checked out yet.<br/>
-                </i></center>
-            @endif
+            <div class="widget-container fluid-height padded">
+                @if($workers->count() != 0)
+                    <table class="table table-striped table-hover table-condensed">
+                        <thead>
+                            <th>Checkout Date</th>
+                            <th>Name</th>
+                        </thead>
+                        <tbody>
+                            @foreach($workers as $w)
+                                <tr>
+                                    <td>{{$w->purchased_at}}</td>
+                                    <td><a href="/{{$w->username}}">{{$w->fullName}}</a></td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                @else
+                    <center><i>
+                        No workers checked out yet.<br/>
+                    </i></center>
+                @endif
+            </div>
         </div>
     </div>
 </section>
